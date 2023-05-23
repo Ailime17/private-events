a994eb3e61226c779bc36284575d1354769353c8
Emilia <emilia.skrzypczak.es@gmail.com>
2023-05-23 16:21:14 +0200

    add events#new & events#create methods and the corresponding view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,23 @@
 class EventsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new]
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @user = current_user
+  end
+
+  def create
+    @user = current_user
+    @event = @user.created_events.create(event_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :date, :creator_id)
   end
 end
