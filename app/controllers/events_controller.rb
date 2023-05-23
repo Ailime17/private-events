9d36a279c47ce7c53283e4897d63e7a19428b312
Emilia <emilia.skrzypczak.es@gmail.com>
2023-05-23 16:42:52 +0200

    add events#show method & view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,6 +16,10 @@ class EventsController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   private
   def event_params
     params.require(:event).permit(:title, :description, :location, :date, :creator_id)
