a0b785b789c043ae34298247cf42225802cf5e64
Emilia <emilia.skrzypczak.es@gmail.com>
2023-05-25 13:19:01 +0200

    add list of user's attended events to the users#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @user_events = @user.created_events
+    @user_created_events = @user.created_events
+    @user_attended_events = @user.attended_events
   end
 end
