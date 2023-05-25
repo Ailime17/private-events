2ce07e36758bb61b00946e12ef17afa8b617551d
Emilia <emilia.skrzypczak.es@gmail.com>
2023-05-25 16:28:00 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show] do
-    resources :events
+    resources :events, only: [:new, :create]
   end
+
+  resources :events, except: [:new, :create]
 
   resources :event_attendings, only: :create
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
