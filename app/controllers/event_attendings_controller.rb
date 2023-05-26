1b19fa7655e9c9cffdeeb6fa9a58c897583f6dba
Emilia <emilia.skrzypczak.es@gmail.com>
2023-05-26 16:58:21 +0200

    improve signing up for events

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -1,12 +1,10 @@
 class EventAttendingsController < ApplicationController
 
-  before_action :authenticate_user!, only: :create
-
   def create
     @user = current_user
     @e = Event.find(params[:event_attending][:attended_event_id])
 
-    @e.attendees << @user unless @e.attendees.exists?(@user.id)
+    @e.attendees << @user
     redirect_to event_path(@e)
   end
 end
