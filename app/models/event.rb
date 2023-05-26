2034e83342f0a93013ee271e9cb70a6b20fb51bc
Emilia <emilia.skrzypczak.es@gmail.com>
2023-05-26 11:18:53 +0200

    separate events into expired and upcoming

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendings, source: :event_attendee
+
+  def self.past_events
+    where("date < ?", DateTime.now)
+  end
+
+  def self.future_events
+    where("date >= ?", DateTime.now)
+  end
 end
